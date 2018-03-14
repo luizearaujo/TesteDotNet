a609bf6199934bbc565e43aabec8f544fbde9109
Danilo Oliveira <danilo.t0458.tirrell@xpi.com.br>
2018-03-14 14:36:45 -0300

    Primeiro commit

---
+++ FullFramework/TesteDotNet.MVC/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace TesteDotNet.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public ActionResult Detalhe()
+        {
+            return View();
+        }
+    }
+}
---
+++ NetCore2/TesteDotNet.Core2.0/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TesteDotNet.Core2._0.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public IActionResult Detalhe()
+        {
+            return View();
+        }
+    }
+}
